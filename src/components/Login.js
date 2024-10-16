1aa1d704465de7bcf1f6773238790bca58c10c9d
yyash01 <yjain3.436@gmail.com>
2024-10-16 22:53:29 +0530

    updated placeholders

--- src/components/Login.js
+++ src/components/Login.js
@@ -149,6 +149,7 @@ const Login = () => {
                       autoComplete="email"
                       required
                       ref={email}
+                      placeholder="example@domain.com"
                       className="p-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus:outline-none"
                     />
                   </div>
@@ -168,6 +169,7 @@ const Login = () => {
                       autoComplete="current-password"
                       required
                       ref={password}
+                      placeholder="Min 4 characters, atleast 1 digit, 1 lowercase, 1 uppercase"
                       className="p-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 focus:outline-none"
                     />
                   </div>
